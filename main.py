5637fcac7e965661be32edeadc65b86f567c3777
Moldybot9411 <joewhite8844@gmail.com>
2026-02-21 16:11:21 +0100

    feat: switch to openrouter instead of gemini

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from nc_py_api import Nextcloud
 from dotenv import load_dotenv
-from google import genai
+from openrouter import OpenRouter
 from markitdown import MarkItDown
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from premailer import transform
 
 import json
 import os
 import time
 import markdown
+import smtplib
 
 load_dotenv()
 
-gemini_client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+AI_MODEL = "openai/gpt-oss-120b"
+
 nc = Nextcloud(nextcloud_url = os.getenv("NEXTCLOUD_URL"), nc_auth_user = os.getenv("NEXTCLOUD_USER"), nc_auth_pass = os.getenv("NEXTCLOUD_PASS"))
 md = MarkItDown()
-
-# for m in gemini_client.models.list():
-#     for action in m.supported_actions:
-#         if action == "generateContent":
-#             print(m.name)
-
-# quit()
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+emails = []
 
 def compare_map(old_map, new_map):
     new_items = {
@@ -100,12 +101,20 @@ def wait():
 
 if __name__ == "__main__":
     if not os.path.exists("./map.json"):
-        new_map = build_map("e2fi4/BFK-B/")
+        new_map = build_map(os.getenv("NEXTCLOUD_DIR"))
         with open("map.json", "w", encoding="utf8") as fs:
             fs.write(json.dumps(new_map, indent=4))
 
     while True:
-        new_map = build_map("e2fi4/BFK-B/")
+        try:
+            with open("./mailinglist.json", "r", encoding="utf-8") as fs:
+                emails = json.load(fs)
+        except:
+            print("No mailing list specified. Please create 'mailinglist.json' and fill it with an array of target mails!")
+            wait()
+            continue
+
+        new_map = build_map(os.getenv("NEXTCLOUD_DIR"))
         old_map = {}
         diff = {}
 
@@ -123,9 +132,47 @@ if __name__ == "__main__":
         with open("map.json", "w", encoding="utf8") as fs:
             fs.write(json.dumps(new_map, indent=4))
 
+        new_file_names = diff["files"]
+
+        with OpenRouter(
+            api_key=os.getenv("OPENROUTER_API_KEY")
+        ) as client:
+            response = client.chat.send(
+                model = AI_MODEL,
+                messages = [
+                    {"role": "user", "content": """
+                    Die folgenden Dateinamen könnten Teile von Hausaufgaben enthalten, es könnten aber auch unbrauchbare darunter sein.
+                    Filtere Dateinamen heraus, die sicher nicht Teile von Hausaufgaben sind.
+                    Abgaben und Lösungen gehören definitiv nicht zu Hausaufgaben und sollen aus der Liste herausgefiltert werden.
+                    Hier sind die Dateinamen:
+                    
+                    """ + "\n".join(new_file_names)}
+                ],
+                response_format = {
+                    "type": "json_schema",
+                    "json_schema": {
+                        "name": "file_names",
+                        "strict": True,
+                        "schema": {
+                            "type": "object",
+                            "properties": {
+                                "files": {
+                                    "type": "array",
+                                    "description": "File names which are part of Homework"
+                                }
+                            },
+                            "required": ["files"],
+                            "additionalProperties": False
+                        }
+                    }
+                }
+            )
+            print(response.choices[0].message.content)
+            new_file_names = json.loads(response.choices[0].message.content)["files"]
+
         os.makedirs("./download", exist_ok=True)
 
-        for new_file in diff["files"]:
+        for new_file in new_file_names:
             filename = new_file.split("/")[-1]
 
             with open(f"download/{filename}", "wb") as fs:
@@ -140,38 +187,61 @@ if __name__ == "__main__":
                     uploading_files.append(f"{md_file.title}:\n")
                     uploading_files.append(md_file.text_content)
                 except Exception as e:
-                    print(e)
-                
-                try:
-                    gem_file = gemini_client.files.upload(file=f"./download/{file}", config={"mime_type": "text/plain"})
-                    uploading_files.append(gem_file)
-                except Exception as e:
-                    print(e)
+                    print(f"ERROR CONVERTING {file} to MD: {e}")
 
+        result = ""
 
-        result = gemini_client.models.generate_content(
-            model="gemini-3-flash-preview",
-            contents=[
-                *uploading_files,
-                "\n\n",
-                """
-                Diese Datei/en enthält/enthalten eventuell Hausaufgaben.
-                Falls du hier Hausaufgaben erkennst, arbeite den Arbeitsauftrag heraus und verarbeite ihn zu einer vollständigen Lösung.
-                Gib mir die pure Lösung zu einzelnen Aufgaben, ohne Rückfragen.
-                Schreibe die Namen der Dateien an den Anfang deiner Antwort.
-                Ich kenne den Kontext der Hausaufgaben nicht, erkläre mir also kurz und knapp die Kernthemen.
-                Falls kein Arbeitsauftrag vorhanden zu sein scheint, schreibe einfach nur 'kamma nix machen'.
-                """,
-            ],
-        )
+        with OpenRouter(
+            api_key=os.getenv("OPENROUTER_API_KEY")
+        ) as client:
+            result = client.chat.send(
+                model=AI_MODEL,
+                messages=[
+                    {"role": "user", "content": """
+                    Die folgenden Informationen enthalten eventuell Hausaufgaben.
+                    Falls du hier Aufgaben erkennst, arbeite den Arbeitsauftrag heraus und verarbeite ihn zu einer vollständigen Lösung.
+                    Gib mir die pure Lösung zu einzelnen Aufgaben, ohne Rückfragen.
+                    Schreibe die Namen der Dateien an den Anfang deiner Antwort.
+                    Ich kenne den Kontext der Hausaufgaben nicht, erkläre mir also kurz und knapp die Kernthemen.
+                    Wenn du dir sicher bist, dass kein Arbeitsauftrag vorhanden ist, schreibe einfach nur 'kamma nix machen'.
+                    
+                    """ + "\n\n".join(uploading_files)}
+                ],
+            )
 
+        print(result.choices[0].message.content)
         with open("./result.txt", "w", encoding="utf-8") as fh:
-            fh.write(result.text)
+            fh.write(result.choices[0].message.content)
 
-        rendered = markdown.markdown(result.text, extensions=['markdown.extensions.tables'])
+        rendered = markdown.markdown(result.choices[0].message.content, extensions=['markdown.extensions.tables'])
+        rendered = transform(rendered)
 
         with open("./output.html", "w", encoding="utf-8") as fh:
             fh.write(rendered)
+
+        for mail in emails:
+            try:
+                msg = MIMEMultipart("alternative")
+                msg["Subject"] = "Neue Hausaufgaben!!!"
+                msg["From"] = EMAIL_ADDRESS
+
+                text_body = "Bitte öffnen sie diese Mail in einem HTML fähigen client."
+
+                part1 = MIMEText(text_body, "plain")
+                part2 = MIMEText(rendered, "html")
+
+                msg.attach(part1)
+                msg.attach(part2)
+
+                with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+                    smtp.starttls()
+                    smtp.login(EMAIL_ADDRESS, os.getenv("GOOGLE_APP_PASSWORD"))
+                    msg["To"] = mail
+                    smtp.send_message(msg)
+                
+                print(f"Email to '{mail}' was sent")
+            except Exception as e:
+                print(e)
 
         cleanup()
 
